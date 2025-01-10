2c7b4ee09a6513a83d27fdf622b0770ef2ea8844
Pavlo Denysov <pavlo.denysov@vizio.com>
2025-01-10 14:48:03 -0600

    Enable LSP for Bash, Json and C++

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "bashls", "html", "cssls", "jsonls", "clangd" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
