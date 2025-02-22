4566355b649a03896b5599d33df4bdbe4d2a0328
Pavlo Denysov <pavlo.denysov@vizio.com>
2025-02-21 18:28:00 -0600

    Map F2 to toggle wrap

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,6 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", "<F2>", ":set wrap!<CR>:set wrap?<CR>", { desc = "Toggle wrap" })
+
